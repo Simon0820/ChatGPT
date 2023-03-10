a5dc41df14d81c8f611a99b92ad1fa1bc3817710
Simon Garding <91874587+Simon0820@users.noreply.github.com>
2023-03-10 17:07:45 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,24 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      newTodo: '',
+      todos: []
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push({
+          text: this.newTodo,
+          completed: false
+        });
+        this.newTodo = '';
+      }
+    },
+    removeTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  }
+});
 
+app.mount('#app');
